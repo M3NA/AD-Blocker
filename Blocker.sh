c26ce3864c015d87e29feec0295a279f88ce1b50
M3NA <menamaged150@gmail.com>
2021-07-19 03:26:05 +0200

    Added Mac os Support and Some other Fixes

--- Blocker.sh
+++ Blocker.sh
@@ -1,17 +1,33 @@
 
+echo "Detecting System type"
+sys=$(uname)
+while [ "$sys" == "Linux" ]
+do
+echo "Your System is Linux"
+hosts="/etc/hosts"
+break
+done
+
+while [ "$sys" == "Darwin" ]
+do
+echo "Your System is Mac OS"
+hosts="/private/etc/hosts"
+break
+done
+
+while [ $hosts != "" ]
+do
+echo " Starting Service"
+echo "hosts file location is '$hosts'"
+echo "Date: $(date)" > $hosts
 url1="https://adaway.org/hosts.txt"
-
 url2="https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
-
 url3="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-
 echo "Fetching $url1"
-curl $url1 >> hosts
+sudo curl $url1 >> "$hosts"
 echo "Fetching $url2"
-curl $url2 >> hosts
-echo "zbi manga2" >> hosts
+sudo curl $url2 >> "$hosts"
 echo "Fetching $url3"
-curl $url3 >> hosts
-
-
-
+sudo curl $url3 >> "$hosts"
+break
+done
